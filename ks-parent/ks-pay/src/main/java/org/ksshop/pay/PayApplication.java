aac8bcf957c9ddaaaf9878d87771065c8aa4b8c5
Administrator <289802612@qq.com>
2020-04-08 10:44:48 +0800

    A

---
+++ ks-parent/ks-pay/src/main/java/org/ksshop/pay/PayApplication.java
@@ -0,0 +1,15 @@
+package org.ksshop.pay;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(PayApplication.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run(args);
+    }
+}
