789ae32fa714a9b3b824f7a3277668be188b641c
Simon <Simon@DESKTOP-31A5EN6>
2020-04-09 16:22:32 +0800

    A

--- ks-pay/src/main/java/com/ksshop/pay/PayApplication.java
+++ ks-pay/src/main/java/com/ksshop/pay/PayApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(PayApplication.class);
-        springApplication.setBannerMode(Banner.Mode.OFF);
-        springApplication.run(args);
-    }
+	public static void main(String[] args) {
+		SpringApplication springApplication = new SpringApplication(PayApplication.class);
+		springApplication.setBannerMode(Banner.Mode.OFF);
+		springApplication.run(args);
+	}
 }
